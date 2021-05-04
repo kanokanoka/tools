f0f16017c0d08d67dd46b3c780e30a7cd1076fb8
kanokanoka <kanononn@hotmail.com>
2021-05-04 23:19:41 +0900

    change bucket name from $1 to current directory name.

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ macなので.DS_Storeファイルを除外する処理をいれた
 最初は aws s3 cpを使っていたが、aws s3 syncの--deleteを使いたかったのでこうした。
 
 `[仕様]`  
-`sh upload.sh [bucket name]`  
+`sh upload.sh`  
 aws s3 の[bucket name]に対して、現在のフォルダにあるファイルを全部同期する。  
+[bucket name]は、実行時のパスのカレントディレクトリ名を勝手に使う
+(カレントディレクトリがaaaであれば、bucket nameはaaaとして実行
 upload.sh自体と、recursiveに.DS_Storeを除外する。
--- upload.sh
+++ upload.sh
@@ -1,15 +1,14 @@
 # this script is to upload all files to aws s3.
 # it needs directory name equals to bucket name.
-#
-# [input]
-# $1 = s3's bucket of aws
 
+
+current=$(basename `pwd`)
 echo ----check before----
-aws s3 ls s3://$1/
+aws s3 ls s3://$current/
 
 echo ----upload----
 # need to add the file you wants to exclude.
-aws s3 sync ../$1 s3://$1 --exclude "upload.sh" --exclude "*/.DS_Store" --exclude ".DS_Store" --delete
+aws s3 sync ../$current s3://$current --exclude "upload.sh" --exclude "*/.DS_Store" --exclude ".DS_Store" --delete
 
 # search all files from current directory.
 files=`find . -type f`
@@ -20,9 +19,9 @@ file2=`echo $file | awk '{print substr($0, 3)}'`
 echo $file2
 file2_basename=`basename $file2`
 if [ $file2 != "upload.sh" -a $file2_basename != ".DS_Store" ]; then
-  aws s3api put-object-acl --acl public-read --bucket $1 --key $file2
+  aws s3api put-object-acl --acl public-read --bucket $current --key $file2
 fi
 done
 
 echo ----check after----
-aws s3 ls s3://$1/
+aws s3 ls s3://$current/
